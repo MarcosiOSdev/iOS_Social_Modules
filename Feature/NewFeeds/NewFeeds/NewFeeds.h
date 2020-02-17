52ff48e3d7cd7e8af3ebb3a81b5f2cbaea4de20e
Marcos Felipe <mfelipesp@gmail.com>
2020-02-17 19:08:26 -0300

    initing

---
+++ Feature/NewFeeds/NewFeeds/NewFeeds.h
@@ -0,0 +1,19 @@
+//
+//  NewFeeds.h
+//  NewFeeds
+//
+//  Created by Marcos Felipe Souza on 17/02/20.
+//  Copyright © 2020 Marcos Felipe Souza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NewFeeds.
+FOUNDATION_EXPORT double NewFeedsVersionNumber;
+
+//! Project version string for NewFeeds.
+FOUNDATION_EXPORT const unsigned char NewFeedsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewFeeds/PublicHeader.h>
+
+
---
+++ SocialModule/SocialModule/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialModule
+//
+//  Created by Marcos Felipe Souza on 17/02/20.
+//  Copyright © 2020 Marcos Felipe Souza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
